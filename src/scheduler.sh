55fb9ef997fa7ee470504d9749a063116f398960
Steven Massaro <steven.massaro.web@gmail.com>
2024-01-22 21:01:46 -0600

    add tests

--- src/scheduler.sh
+++ src/scheduler.sh
@@ -3,18 +3,7 @@ echo "$(date) - start scheduler"
 while :; do
     echo "$(date) - execute"
 
-    eval "$COMMAND"
-    ret=$?
-
-    if [ -n "$AFTER_COMMAND" ]
-    then
-        if [ $ret -ne 0 ];
-        then
-            echo "$(date) - command failed with exit code $ret, not executing AFTER_COMMAND"
-        else
-            eval "$AFTER_COMMAND"
-        fi
-    fi
+    execute.sh
 
     echo "$(date) - sleep for ${DELAY:-1d}"
     sleep "${DELAY:-1d}"
